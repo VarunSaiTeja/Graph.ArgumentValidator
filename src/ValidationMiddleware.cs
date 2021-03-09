fbd74c1783ff568852388bc877b0d7a93b5dde21
VarunSaiTeja <35555010+VarunSaiTeja@users.noreply.github.com>
2021-03-10 00:04:00 +0530

    Returning field name, path in errors respone

--- src/ValidationMiddleware.cs
+++ src/ValidationMiddleware.cs
@@ -3,13 +3,19 @@ using HotChocolate.Resolvers;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Graph.ArgumentValidator
 {
     internal class ValidationMiddleware
     {
+        internal class ErrorDetail
+        {
+            public string ErrorMessage;
+            public string Field;
+            public string Path;
+        }
+
         private readonly FieldDelegate _next;
 
         public ValidationMiddleware(FieldDelegate next)
@@ -20,7 +26,8 @@ namespace Graph.ArgumentValidator
         // this middleware is ensured to only execute on fields that have arguments that need validation.
         public async Task InvokeAsync(IMiddlewareContext context)
         {
-            var errors = new List<ValidationResult>();
+            var validationResults = new List<ValidationResult>();
+            var errors = new List<ErrorDetail>();
 
             // we could even further optimize and aggregate this list in the interceptor and inject it into the middleware
             foreach (var argument in context.Field.Arguments)
@@ -30,18 +37,33 @@ namespace Graph.ArgumentValidator
                 {
                     var input = context.ArgumentValue<object>(argument.Name);
                     var validationContext = new ValidationContext(input);
-                    validate(input, validationContext, errors);
+                    validate(input, validationContext, validationResults);
+
+                    if (validationResults.Any())
+                    {
+                        foreach (var validationResult in validationResults)
+                        {
+                            errors.Add(new ErrorDetail
+                            {
+                                ErrorMessage = validationResult.ErrorMessage,
+                                Field = validationResult.MemberNames.FirstOrDefault() ?? argument.Name,
+                                Path = argument.Name
+                            });
+                        }
+                        validationResults.Clear();
+                    }
                 }
             }
 
-            if (errors.Count > 0)
+            if (errors.Any())
             {
                 foreach (var error in errors)
                 {
                     // NOTE: it would be good to provide an error code.
                     context.ReportError(ErrorBuilder.New()
                         .SetMessage(error.ErrorMessage)
-                        // .SetCode()
+                        .SetExtension("field", char.ToLowerInvariant(error.Field[0]) + error.Field.Substring(1))
+                        .SetPath(Path.New(error.Path))
                         .Build());
                 }
             }
